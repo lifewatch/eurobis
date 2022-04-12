ae2a8ca7441d05706abffe24fa85190ed0179f0e
salvafern <salvador.fernandez@vliz.be>
2022-04-12 15:26:15 +0000

    add simple browser function to download toolbox

---
+++ R/helper.R
@@ -0,0 +1,19 @@
+#' Browse the LifeWatch/EMODnet-Biology Download Toolbox on your browser
+#'
+#' @return NULL, only opens the Download Toolbox in your browser.
+#' @export
+#'
+#' @examples eurobis_download_toolbox()
+eurobis_download_toolbox <- function(){
+  url <- eurobis_url$info$toolbox
+  req <- httr::HEAD(url)
+  httr::stop_for_status(req)
+  
+  if(interactive()){
+    message("The LifeWatch/EMODnet-Biology Download Toolbox provides a web interface to fine tune the selection of data from EurOBIS. At the end of the selection there is a button named \"Get webservice url\". Copy&Paste this URL into eurobis_occurrences(url = \"<URL>\"). Press ENTER to continue:" )
+    readline("")
+    browseURL(url)
+  }
+  
+  NULL
+}
